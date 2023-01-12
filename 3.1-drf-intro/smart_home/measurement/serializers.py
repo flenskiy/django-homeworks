572d20a5c10e7e4f581c2351511c36776ae3bf17
administrator <P@ssw0rd>
2023-01-12 12:37:47 +0300

    final commit

--- 3.1-drf-intro/smart_home/measurement/serializers.py
+++ 3.1-drf-intro/smart_home/measurement/serializers.py
@@ -1,3 +1,29 @@
 from rest_framework import serializers
 
-# TODO: опишите необходимые сериализаторы
+from .models import Measurement, Sensor
+
+
+class SensorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description']
+
+
+class MeasurementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = ['temperature', 'created_at']
+
+
+class SensorDetailSerializer(serializers.ModelSerializer):
+    measurements = MeasurementSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description', 'measurements']
+
+
+class MeasurementCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = ['id', 'sensor', 'temperature', 'created_at']
--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from .views import SensorsView, SensorDetailView, MeasurementCreateView
+
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', SensorsView.as_view()),
+    path('sensors/<pk>/', SensorDetailView.as_view()),
+    path('measurements/', MeasurementCreateView.as_view()),
 ]
